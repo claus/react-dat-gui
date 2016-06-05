adbe7b6bf8eb43bd2fd42fdd62c07af48d91b143
Claus Wahlers <claus@codeazur.com.br>
2016-06-05 03:56:35 -0300

    make DatButton a stateless function component

--- src/components/DatButton.jsx
+++ src/components/DatButton.jsx
@@ -1,29 +1,14 @@
 import React, { PropTypes } from 'react';
 
-class DatButton extends React.Component {
+const DatButton = ({ label, onClick }) => (
+    <li className="cr button">
+        <span onClick={onClick}>{label}</span>
+    </li>    
+);
 
-    static propTypes = {
-        label: PropTypes.string,
-        onClick: PropTypes.func,
-    };
-
-    constructor(props, context) {
-        super(props, context);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(event) {
-    }
-
-    render() {
-        const { label } = this.props;
-        return (
-            <li className="cr button">
-                <span onClick={this.props.onClick}>{label}</span>
-            </li>
-        );
-    }
-
-}
+DatButton.propTypes = {
+    label: PropTypes.string,
+    onClick: PropTypes.func,
+};
 
 export default DatButton;
