9223af18ead694d4db515ff68c7fa6699bb6bf90
Claus Wahlers <claus@codeazur.com.br>
2019-02-28 20:35:15 +0100

    bumped to 2.2.0, npm run build

--- build/react-dat-gui.js
+++ build/react-dat-gui.js
@@ -6443,6 +6443,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = __webpack_require__(0);
@@ -6458,6 +6460,8 @@ var _classnames = __webpack_require__(50);
 var _classnames2 = _interopRequireDefault(_classnames);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -6489,12 +6493,24 @@ var DatFolder = function (_Component) {
   }
 
   _createClass(DatFolder, [{
+    key: 'renderChildren',
+    value: function renderChildren() {
+      // Disable this rule to take title out of the props so nested folders can have unique titles.
+      // eslint-disable-next-line no-unused-vars
+      var _props = this.props,
+          children = _props.children,
+          title = _props.title,
+          rest = _objectWithoutProperties(_props, ['children', 'title']);
+
+      return _react2.default.Children.map(children, function (child) {
+        return (0, _react.cloneElement)(child, _extends({}, rest));
+      });
+    }
+  }, {
     key: 'render',
     value: function render() {
       var closed = this.state.closed;
-      var _props = this.props,
-          title = _props.title,
-          children = _props.children;
+      var title = this.props.title;
 
 
       return _react2.default.createElement(
@@ -6511,7 +6527,7 @@ var DatFolder = function (_Component) {
           _react2.default.createElement(
             'ul',
             null,
-            children
+            this.renderChildren()
           )
         )
       );
