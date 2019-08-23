506b435c70a34c2aa04efba16c255b208e955cc3
Tim Ellenberger <timellenberger@gmail.com>
2019-08-23 12:44:13 -0700

    Match prev example site styling

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,5 +1,26 @@
+import Head from 'next/head';
 import ReactDatGui from '../components/ReactDatGui';
 
-const Page = () => <ReactDatGui />;
+const Page = () => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+    </Head>
+    <style jsx global>{`
+      html {
+        width: 100%;
+        height: 100%;
+      }
+      body {
+        margin: 0 20px;
+        background-color: #f3eee8;
+        color: #53504d;
+        font: 14px/22px Arial, sans-serif;
+      }
+    `}</style>
+    <ReactDatGui />
+  </>
+);
 
 export default Page;
